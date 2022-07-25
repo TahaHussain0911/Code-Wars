c42a04994e868ae828f9b0a2f5220a8895bf392a
Taha Hussain <100864222+TahaHussain0911@users.noreply.github.com>
2022-07-25 14:21:19 +0500

    Update camelcasing.js

--- 7-kyu/camelcasing.js
+++ 7-kyu/camelcasing.js
@@ -1,3 +1,7 @@
+
+//Divides word depending on the first capital letter
 function sol(string) {
     return string.replace(/([A-Z])/g, " $1")
 }
+
+console.log(sol("ScarletWitch")) //output:Scarlet Witch
