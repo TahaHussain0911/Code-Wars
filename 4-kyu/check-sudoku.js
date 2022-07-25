8f816755f3e86f64fca40ce2629a0dea6787016d
Taha Hussain <100864222+TahaHussain0911@users.noreply.github.com>
2022-07-25 15:16:09 +0500

    Create check-sudoku.js

---
+++ 4-kyu/check-sudoku.js
@@ -0,0 +1,59 @@
+function rowcheck(funcarr) {
+    for (let i = 0; i < funcarr.length; i++) {
+        let sorted = funcarr[i].sort();
+        for (let j = 0; j < funcarr.length; j++) {
+            if (sorted[j] == sorted[j + 1]) {
+                return false
+            }
+        }
+    }
+    return true
+}
+function colcheck(func2arr){
+    let answer=[];
+    for (let i = 0; i < func2arr.length; i++) {
+        let newarr=[];
+        for (let j = 0; j < func2arr.length; j++) {
+            newarr.push(func2arr[j][i])
+        }
+        answer.push(newarr)
+    }
+    return rowcheck(answer)
+}
+function validSolution(board){
+    return colcheck(board)&&rowcheck(board);
+}
+console.log(validSolution([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  ])) //output=false
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]))//output=false
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]))//output=true
